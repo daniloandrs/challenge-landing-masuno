92210da2af0ade2f03578ad6b8f518ac00407555
Danilo Andres M <danilo.marcelo@andheuris.com>
2024-11-11 22:28:54 -0500

    get ip

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,18 @@ import { PreloadAllModules, provideRouter, withDebugTracing, withPreloading } fr
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
       // withDebugTracing(),
       withPreloading(PreloadAllModules)
-    ), 
-    provideClientHydration()
+    ),
+    provideClientHydration(),
+    provideHttpClient()
+
   ]
 };
--- src/modules/landing/ui/components/banner/banner.component.ts
+++ src/modules/landing/ui/components/banner/banner.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { TrazoTitleComponent } from '../trazo-title/trazo-title.component';
 import { CouponComponent } from '../coupon/coupon.component';
-
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { IpServiceService } from '@/modules/landing/services/ip.service';
 @Component({
   selector: 'landing-banner',
   standalone: true,
@@ -13,5 +14,18 @@ import { CouponComponent } from '../coupon/coupon.component';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+
+  ip!:string;
+
+  constructor(private ipService:IpServiceService) { }
+  async ngOnInit() {}
+
+
+  async getIp() {
+    this.ipService.getIPAddress().subscribe( ({ip}) => {
+      console.log(ip)
+      this.ip = ip;
+    });
+  }
 
 }
